Fixed major error in variance, updated master_df

create_medpc_master.py
@@ -1,20 +1,23 @@
 import pandas as pd
 import numpy as np
 import os
-import datetime
 import math
-from tkinter.filedialog import askdirectory
+import datetime
 
-# Returns dictionary with Protocol, Reward, Lever, Lick, IPI, Variance
+# Returns dictionary with Protocol, Run Time, Reward, Lever, Lick, IPI, Variance
+# Added run time
 def medpc_extract(file):
     # file = home_dir + '/' + file
     with open(file) as f:
-        print(f)
         file_info = f.readlines()
-        medpc_data = dict.fromkeys(['Reward', 'Lever', 'Lick', 'IPI', 'Variance'])
+        medpc_data = dict.fromkeys(['Protocol', 'Run Time', 'Reward', 'Lever', 'Lick', 'IPI', 'Variance'])
         medpc_data['Protocol'] = file_info[12][5:-1]
         letters = ['F:','G:','U:','W:','X:','Y:','Z:']
         
+        # Find run time
+        start_time = datetime.datetime.strptime(file_info[10][-9:-1],'%H:%M:%S')
+        end_time = datetime.datetime.strptime(file_info[11][-9:-1],'%H:%M:%S')
+        medpc_data['Run Time'] = f'{end_time - start_time}'
         row_nums = []
         for letter in letters: #This IF statement catches the case when there is no data for a requested letter. Places a 0.
             row=[ind for ind, row in enumerate(file_info) if row == f'{letter}\n']
@@ -23,31 +26,39 @@ def medpc_extract(file):
             else:
                 row_nums += [0]
         boundaries = dict(zip(letters, row_nums))
-        medpc_data['Reward'] = np.asarray([float(item) for row in file_info[(boundaries['Z:']+1):]
-                                           for item in row[7:-1].split() ])
-        medpc_data['Lever'] = np.asarray([float(item) for row in file_info[(boundaries['X:']+1):boundaries['Y:']] for item in row[7:-1].split() ])
-        medpc_data['Lick'] = np.asarray([float(item) for row in file_info[(boundaries['Y:']+1):boundaries['Z:']] for item in row[7:-1].split() ])
+        try:
+            medpc_data['Reward'] = np.asarray([float(item) for row in file_info[(boundaries['Z:']+1):]
+                                       for item in row[7:-1].split() ])
+            medpc_data['Lever'] = np.asarray([float(item) for row in file_info[(boundaries['X:']+1):boundaries['Y:']] for item in row[7:-1].split() ])
+            medpc_data['Lick'] = np.asarray([float(item) for row in file_info[(boundaries['Y:']+1):boundaries['Z:']] for item in row[7:-1].split() ])
+        except: # Added these exceptions because 4241's file on 12/12 was giving me trouble
+            print(f'Error in file: {file}')
         if 'FR5' in medpc_data['Protocol']:
             if not boundaries['F:']==0: #this catches the one day recorded before I started saving the IPIs in F
-                medpc_data['IPI'] = np.asarray(
-                    [float(item) for row in file_info[(boundaries['F:'] + 1):boundaries['G:']] for item in row[7:-1].split()])
-                medpc_data['Variance'] = np.asarray(
-                    [float(item) for row in file_info[(boundaries['U:'] + 1):boundaries['W:']] for item in
-                     row[7:-1].split()])
+                try:
+                    medpc_data['IPI'] = np.asarray(
+                        [float(item) for row in file_info[(boundaries['F:'] + 1):boundaries['G:']] for item in row[7:-1].split()])
+                    medpc_data['Variance'] = np.asarray(
+                        [float(item) for row in file_info[(boundaries['U:'] + 1):boundaries['W:']] for item in
+                         row[7:-1].split()])
+                except:
+                    print(file)
+                    medpc_data['Reward'] == []
+                    medpc_data['Lever'] == []
+                    medpc_data['Lick'] == []
     return medpc_data
 
 
 
 def create_medpc_master(mice, dates):
-    columns = ['Mouse', 'Date', 'Protocol', 'Protocol Day', 'Run Time', 'Reward', 'Lever', 'Lick', 'IPI', 'Variance']
+    columns = ['Mouse', 'Date', 'Protocol', 'Run Time', 'Reward', 'Lever', 'Lick', 'IPI', 'Variance']
     total_mice = len(mice)
     days_training=len(dates)
-    mice_x_day = [y  for x in mice for y in [x]*days_training] #this only works when all mice contribute to all days
+    mice_x_day = [y for x in mice for y in [x]*days_training] #this only works when all mice contribute to all days
     medpc_master = pd.DataFrame(columns = columns)
     medpc_master['Mouse'] = mice_x_day
     medpc_master['Date'] = [y for x in range(total_mice) for y in dates]
-    file_dir = askdirectory(title='Select Folder: ',
-                            initialdir='C:/Users/...')
+    file_dir = "/Users/emma-fuze-grace/Lab/Behavior_VarSeq/Medpc Data"
     os.chdir((file_dir))
     print(f'Now in following directory: {file_dir}')
     for f in os.listdir():
@@ -55,7 +66,6 @@ def create_medpc_master(mice, dates):
         file_name, file_ext = os.path.splitext(f)
         file_date = file_name[:4]+file_name[5:7]+file_name[8:10]
         mouse_num = int(file_name[-4:])
-       # with open(f, 'r') as ff:
         medpc_data = medpc_extract(f)
         curr_mouse = medpc_master[medpc_master['Mouse']==mouse_num].index
         curr_date = medpc_master[medpc_master['Date']==file_date].index
@@ -67,11 +77,30 @@ def create_medpc_master(mice, dates):
             medpc_master.at[ind, 'Lick'] = medpc_data['Lick']
             medpc_master.at[ind, 'IPI'] = medpc_data['IPI']
             medpc_master.at[ind, 'Variance'] = medpc_data['Variance']
-
+            medpc_master.at[ind, 'Run Time'] = (medpc_data['Run Time'])
+    
+    # Add IPI/Var data for 4225 (First FR5 mouse)
+    mouse_4225_ind = medpc_master[medpc_master['Mouse']==4225].index
+    date_12_04_ind = medpc_master[medpc_master['Date']=='20211204'].index
+    new_ind = int(mouse_4225_ind.intersection(date_12_04_ind).values)
+    lever_4225 = medpc_master.at[new_ind, 'Lever']
+    reward_4225 = medpc_master.at[new_ind, 'Reward']
+    IPI_4225 = [lever_4225[i] - lever_4225[i-1] for i in range(1,len(lever_4225))]
+    Var_4225 = []
+    for i in range(len(reward_4225)):
+        curr_reward = reward_4225[i]
+        sequence = lever_4225[lever_4225 < curr_reward][-5:]
+        Var_4225.append(np.var(sequence))
+    medpc_master.at[new_ind, 'IPI'] = IPI_4225
+    medpc_master.at[new_ind, 'Variance'] = Var_4225
     return medpc_master
 
 mice=[4217,4218,4219,4220,4221,4222,4223,4224,4225,4226,4227,4228,
       4229,4230,4231,4232,4233,4234,4235,4236,4237,4238,4239,4240,4241,4242,4243, 4244] #(ints)
 dates=['20211202', '20211203', '20211204', '20211205', '20211206', '20211207', '20211208',
        '20211209', '20211210', '20211211', '20211212', '20211213', '20211214', '20211215'] #(strs)
-master_df = create_medpc_master(mice,dates)
+
+
+
+
+

mean_med_variance.py
@@ -1,9 +1,7 @@
 import matplotlib.pyplot as plt
-import scipy.stats as stats
 import numpy as np
 import math
 import matplotlib
-from statistics import mean
 from create_medpc_master import create_medpc_master
 matplotlib.rcParams['pdf.fonttype'] = 42
 matplotlib.rcParams['ps.fonttype'] = 42
@@ -15,7 +13,6 @@ dates=['20211202', '20211203', '20211204', '20211205', '20211206', '20211207', '
 master_df = create_medpc_master(mice,dates)
 
 # Had a problem with extracting data from '2021-12-12_16h47m_Subject 4241.txt'
-# Also we are missing one file still, so these graphs might not give the full picture
 
 # Taken from Reward_across_training
 discard_list = [4217, 4218, 4221, 4227, 4232, 4235, 4236, 4237, 4238, 4244]     
@@ -24,30 +21,46 @@ for mouse in discard_list:
     indices.append(master_df[master_df['Mouse']==mouse].index)
 indices=[x for l in indices for x in l]
 master_df=master_df.drop(indices, axis=0)
-
 num_days_training = len(np.unique(master_df['Date']))
 mice = np.unique(master_df['Mouse'])  
 
-# Contains median session variance for each mouse/day combo    
+# Contains median session variance for each mouse/day combo   
 Variance_FR5 = [[] for i in range(num_days_training - 2)]
 Variance_FR5var = [[] for i in range(num_days_training - 2)]
+
+fig,ax=plt.subplots(1,1)
 
 for i in range(len(mice)):
     curr_mouse = mice[i]
     curr_mouse_df = (master_df[master_df['Mouse']==curr_mouse]).reset_index()
-    curr_mouse_variance = (curr_mouse_df['Variance'])
-    mouse_group = curr_mouse_df.iloc[13]['Protocol'][41:] #Grab last day protocol to put mouse in group
-    for j in range(num_days_training):
-        try:
-            curr_variance = curr_mouse_variance[j]
-            if curr_variance is not None:
-                # Can't find file for 4234 on the 13th -> so check if nan
-                if 'va2' not in mouse_group and not math.isnan(np.mean(curr_variance)):
-                    Variance_FR5[j - 2].append(np.median(curr_variance))
-                elif not math.isnan(np.mean(curr_variance)):
-                    Variance_FR5var[j - 2].append(np.median(curr_variance))
-        except:
-            print(f'Error finding variance data for {curr_mouse}')
+    exclude_FR1 = [curr_mouse_df['Variance'][ind] is not None for ind in range(len(curr_mouse_df))]
+    curr_mouse_df = (curr_mouse_df[exclude_FR1]).reset_index()
+    curr_mouse_variance = curr_mouse_df['Variance']
+    mouse_group = curr_mouse_df.iloc[-1]['Protocol'][41:] #Grab last day protocol to put mouse in group
+    if 'va2' not in mouse_group:
+        plt.plot([np.mean(day) for day in curr_mouse_variance], linewidth = 1, linestyle = '--', color = 'tomato')
+    else:
+        plt.plot([np.mean(day) for day in curr_mouse_variance], linewidth = 1, linestyle = '--', color = 'cornflowerblue')
+    for j in range(curr_mouse_variance.size):
+        curr_variance = curr_mouse_variance[j]
+        # Can't find file for 4234 on the 13th -> so check if nan
+        if 'va2' not in mouse_group and not math.isnan(np.median(curr_variance)):
+            Variance_FR5[j].append(np.median(curr_variance))
+        elif not math.isnan(np.median(curr_variance)):
+            Variance_FR5var[j].append(np.median(curr_variance))
+        
+
+plt.vlines(3.5,0,70, color='k', linestyle='dashed')
+plt.xlabel('Time from first FR5 session (day)', size=16)
+plt.xticks(fontsize=14)
+plt.ylabel('Individual session variance medians', size=16)
+plt.yticks(fontsize=14)
+plt.legend(['FR5, N=10 mice', 'Var, N=8 mice'], loc='upper right')
+leg = ax.get_legend()
+leg.legendHandles[0].set_color('tomato')
+leg.legendHandles[1].set_color('cornflowerblue')
+ax.spines['right'].set_visible(False)
+ax.spines['top'].set_visible(False)
 
 # Take mean of median and median of median
 Variance_means_FR5 = [np.mean(day) for day in Variance_FR5 if day]
@@ -64,19 +77,19 @@ FR5var_lower = [a-b for a,b in zip(Variance_means_FR5var,Variance_sem_FR5var)]
 FR5var_upper = [a+b for a,b in zip(Variance_means_FR5var,Variance_sem_FR5var)]
 
 # Plot Mean of Medians
-# Had to add 1 to FR5 since I need to make the IPI/Var vectors for the first day
+
 fig,ax=plt.subplots(1,1)
-plt.plot([i+1 for i in range(11)], Variance_means_FR5, linewidth=2, color='tomato')
-plt.vlines([i+1 for i in range(11)], FR5_lower, FR5_upper, linewidths=2, colors='tomato') 
+plt.plot(Variance_means_FR5, linewidth=2, color='tomato')
+plt.vlines(range(len(Variance_means_FR5var)), FR5_lower, FR5_upper, linewidths=2, colors='tomato') 
 plt.plot(Variance_means_FR5var, linewidth=2, color='cornflowerblue')
 plt.vlines(range(len(Variance_means_FR5var)), FR5var_lower, FR5var_upper, linewidths=2, colors='cornflowerblue') 
 
-plt.vlines(3.5,0,5, color='k', linestyle='dashed')
+plt.vlines(3.5,0,7, color='k', linestyle='dashed')
 plt.xlabel('Time from first FR5 session (day)', size=16)
 plt.xticks(fontsize=14)
 plt.ylabel('Means of session variance medians', size=16)
 plt.yticks(fontsize=14)
-plt.legend(['FR5, N=10 mice', 'Var, N=8 mice'], loc='lower right')
+plt.legend(['FR5, N=10 mice', 'Var, N=8 mice'], loc='upper right')
 leg = ax.get_legend()
 leg.legendHandles[0].set_color('tomato')
 leg.legendHandles[1].set_color('cornflowerblue')
@@ -85,8 +98,8 @@ ax.spines['top'].set_visible(False)
 
 # Plot Median of Medians
 fig,ax=plt.subplots(1,1)
-plt.plot([i+1 for i in range(11)], Variance_med_FR5, linewidth=2, color='tomato')
-# plt.vlines([i+1 for i in range(11)], FR5_lower, FR5_upper, linewidths=2, colors='tomato') 
+plt.plot(range(len(Variance_means_FR5)), Variance_med_FR5, linewidth=2, color='tomato')
+# plt.vlines(range(len(Variance_means_FR5)), FR5_lower, FR5_upper, linewidths=2, colors='tomato') 
 plt.plot(Variance_med_FR5var, linewidth=2, color='cornflowerblue')
 # plt.vlines(range(len(Variance_means_FR5var)), FR5var_lower, FR5var_upper, linewidths=2, colors='cornflowerblue') 
 
@@ -95,7 +108,7 @@ plt.xlabel('Time from first FR5 session (day)', size=16)
 plt.xticks(fontsize=14)
 plt.ylabel('Median of session variance medians', size=16)
 plt.yticks(fontsize=14)
-plt.legend(['FR5, N=10 mice', 'Var, N=8 mice'], loc='lower right')
+plt.legend(['FR5, N=10 mice', 'Var, N=8 mice'], loc='upper right')
 leg = ax.get_legend()
 leg.legendHandles[0].set_color('tomato')
 leg.legendHandles[1].set_color('cornflowerblue')