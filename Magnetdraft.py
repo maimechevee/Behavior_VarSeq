35a1662431aaeb5034c183cc00db30c2e8af2404
emma-grace <96401377+emma-grace@users.noreply.github.com>
2022-03-15 18:54:10 -0500

    added medpc data, numbers of presses

--- Magnetdraft.py
+++ Magnetdraft.py
@@ -7,191 +7,200 @@ Created on Fri Mar 11 10:18:46 2022
 ##############################################################################
 # Draft
 ##############################################################################
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
+import scipy as sp
+from scipy import stats
+import os
+import math
+
 #Extract data
-filename='H:\\Maxime\\Hall Sensor Data\\20220131\\HallSensor_20220131_4229.csv' #ok
-filename='H:\\Maxime\\Hall Sensor Data\\20211215\\HallSensor_20211215_4229.csv' #ok
-filename='H:\\Maxime\\Hall Sensor Data\\20211214\\HallSensor_20211214_4229.csv' #ok
-filename='H:\\Maxime\\Hall Sensor Data\\20211213\\HallSensor_20211213_4229.csv' #salvageable
-filename='H:\\Maxime\\Hall Sensor Data\\20211212\\HallSensor_20211212_4229.csv' #ok
-filename='H:\\Maxime\\Hall Sensor Data\\20211211\\HallSensor_20211211_4229.csv' #salvageable
-filename='H:\\Maxime\\Hall Sensor Data\\20211210\\HallSensor_20211210_4229.csv' #ok
-filename='H:\\Maxime\\Hall Sensor Data\\20211209\\HallSensor_20211209_4229.csv' #trash
-filename='H:\\Maxime\\Hall Sensor Data\\20211208\\HallSensor_20211208_4229.csv' #trash
-filename='H:\\Maxime\\Hall Sensor Data\\20220126\\HallSensor_20220126_4229.csv' #trash
-filename='H:\\Maxime\\Hall Sensor Data\\20220127\\HallSensor_20220127_4229.csv' #trash
-filename='H:\\Maxime\\Hall Sensor Data\\20220128\\HallSensor_20220128_4229.csv' #trash
-filename='H:\\Maxime\\Hall Sensor Data\\20220130\\HallSensor_20220130_4229.csv' #partially useable
+# filename='H:\\Maxime\\Hall Sensor Data\\20220131\\HallSensor_20220131_4229.csv' #ok
+# filename='H:\\Maxime\\Hall Sensor Data\\20211215\\HallSensor_20211215_4229.csv' #ok
+# filename='H:\\Maxime\\Hall Sensor Data\\20211214\\HallSensor_20211214_4229.csv' #ok
+# filename='H:\\Maxime\\Hall Sensor Data\\20211213\\HallSensor_20211213_4229.csv' #salvageable
+# filename='H:\\Maxime\\Hall Sensor Data\\20211212\\HallSensor_20211212_4229.csv' #ok
+# filename='H:\\Maxime\\Hall Sensor Data\\20211211\\HallSensor_20211211_4229.csv' #salvageable
+# filename='H:\\Maxime\\Hall Sensor Data\\20211210\\HallSensor_20211210_4229.csv' #ok
+# filename='H:\\Maxime\\Hall Sensor Data\\20211209\\HallSensor_20211209_4229.csv' #trash
+# filename='H:\\Maxime\\Hall Sensor Data\\20211208\\HallSensor_20211208_4229.csv' #trash
+# filename='H:\\Maxime\\Hall Sensor Data\\20220126\\HallSensor_20220126_4229.csv' #trash
+# filename='H:\\Maxime\\Hall Sensor Data\\20220127\\HallSensor_20220127_4229.csv' #trash
+# filename='H:\\Maxime\\Hall Sensor Data\\20220128\\HallSensor_20220128_4229.csv' #trash
+# filename='H:\\Maxime\\Hall Sensor Data\\20220130\\HallSensor_20220130_4229.csv' #partially useable
 
-mouse=4229
-date='20220201'
+# mouse=4229
+# date='20220201'
 
-directory='H:/Maxime/Hall Sensor Data/CATEG'
-filename='H://Maxime//Hall Sensor Data//CATEG//20220214//HallSensor_20220214_4407.csv'
-mouse=4407
-skip=['20220208']
-Dates=os.listdir(directory)
-for date in Dates:
-    if date in skip:
-        continue
-    file=[x for x in os.listdir(os.path.join(directory,date)) if str(mouse) in x][0]
-    filename=os.path.join(directory,date, file)
+# directory='H:/Maxime/Hall Sensor Data/CATEG'
+# filename='H://Maxime//Hall Sensor Data//CATEG//20220214//HallSensor_20220214_4407.csv'
+# mouse=4407
+# skip=['20220208']
+# Dates=os.listdir(directory)
+# for date in Dates:
+#     if date in skip:
+#         continue
+#     file=[x for x in os.listdir(os.path.join(directory,date)) if str(mouse) in x][0]
+#     filename=os.path.join(directory,date, file)
 
-    Data=pd.read_csv(filename)
-    Data.columns=['Hall_sensor','Magnet_block','Time']
+#     Data=pd.read_csv(filename)
+#     Data.columns=['Hall_sensor','Magnet_block','Time']
 
 
-    #smooth
-    from scipy.ndimage.filters import uniform_filter1d
-    smooth_data = uniform_filter1d(Data.Hall_sensor, size=50)
-    Data.Hall_sensor=smooth_data
+#     #smooth
+#     from scipy.ndimage.filters import uniform_filter1d
+#     smooth_data = uniform_filter1d(Data.Hall_sensor, size=50)
+#     Data.Hall_sensor=smooth_data
     
-    # truncate data to keep only time when the magnet was on
-    beg_ind, end_ind, rough_baseline = detect_magnet_ON_OFF(Data, plot=False)
+#     # truncate data to keep only time when the magnet was on
+#     beg_ind, end_ind, rough_baseline = detect_magnet_ON_OFF(Data, plot=False)
     
-    Data=Data[beg_ind:end_ind]
+#     Data=Data[beg_ind:end_ind]
     
-    #Flip data if necessary:
-    if Data['Hall_sensor'].values[0] > np.mean(Data['Hall_sensor'].values):
-        Data['Hall_sensor']=Data['Hall_sensor'].values * (-1)
-        rough_baseline = rough_baseline * (-1)
-    # plt.figure()
-    # plt.plot(Data['Hall_sensor'], alpha=0.5)
-    # plt.hlines(rough_baseline,0,10000)
-    # plt.title(date)
+#     #Flip data if necessary:
+#     if Data['Hall_sensor'].values[0] > np.mean(Data['Hall_sensor'].values):
+#         Data['Hall_sensor']=Data['Hall_sensor'].values * (-1)
+#         rough_baseline = rough_baseline * (-1)
+#     # plt.figure()
+#     # plt.plot(Data['Hall_sensor'], alpha=0.5)
+#     # plt.hlines(rough_baseline,0,10000)
+#     # plt.title(date)
 
     
     
-    #print(target, number)
-#result=
-#614 (4229/20220131)
-#638 (4229/20211215)
-#636 (4229/20211214)
-#575 (4229/20211212)
-#700 (4229/20211210)
-#check
-number_of_presses, Down_idx, Up_idx = detect_press(Data,target=700, plot=True)
+#     #print(target, number)
+# #result=
+# #614 (4229/20220131)
+# #638 (4229/20211215)
+# #636 (4229/20211214)
+# #575 (4229/20211212)
+# #700 (4229/20211210)
+# #check
+# number_of_presses, Down_idx, Up_idx = detect_press(Data,target=700, plot=True)
 
-#Adjust indices to start and end at right place, not at threshold crossing
-new_Down_idx, new_Up_idx = adjust_press_idx(Data,Down_idx, Up_idx, plot=True)
+# #Adjust indices to start and end at right place, not at threshold crossing
+# new_Down_idx, new_Up_idx = adjust_press_idx(Data,Down_idx, Up_idx, plot=True)
 
-#Normalize te entire trace
-Data.Hall_sensor=sp.stats.zscore(Data.Hall_sensor.values)
+# #Normalize te entire trace
+# Data.Hall_sensor=sp.stats.zscore(Data.Hall_sensor.values)
 
-#it starts with UP and strats with Down so remove the ends accordingly
-new_Down_idx=new_Down_idx[:-1]
-new_Up_idx=new_Up_idx[1:]
+# #it starts with UP and strats with Down so remove the ends accordingly
+# new_Down_idx=new_Down_idx[:-1]
+# new_Up_idx=new_Up_idx[1:]
 
-#Make a matrix with one press per row
-plt.hist(np.subtract(new_Up_idx,new_Down_idx), bins=100)
-length_of_vector=int(np.mean(np.subtract(new_Up_idx,new_Down_idx)) + np.std(np.subtract(new_Up_idx,new_Down_idx)))
-press_matrix=np.zeros((len(new_Down_idx),length_of_vector ))
-magnet=Data.Hall_sensor.values
-for i,(pressDown,pressUp) in enumerate(zip(new_Down_idx, new_Up_idx)):
-    if pressUp-pressDown>length_of_vector:
-        stop=length_of_vector
-    else:
-        stop=pressUp-pressDown
+# #Make a matrix with one press per row
+# plt.hist(np.subtract(new_Up_idx,new_Down_idx), bins=100)
+# length_of_vector=int(np.mean(np.subtract(new_Up_idx,new_Down_idx)) + np.std(np.subtract(new_Up_idx,new_Down_idx)))
+# press_matrix=np.zeros((len(new_Down_idx),length_of_vector ))
+# magnet=Data.Hall_sensor.values
+# for i,(pressDown,pressUp) in enumerate(zip(new_Down_idx, new_Up_idx)):
+#     if pressUp-pressDown>length_of_vector:
+#         stop=length_of_vector
+#     else:
+#         stop=pressUp-pressDown
         
-    press_matrix[i,:stop]=magnet[pressDown:pressDown+stop]
+#     press_matrix[i,:stop]=magnet[pressDown:pressDown+stop]
     
-plt.imshow( press_matrix)
-pcorr = manual_pairwise_pearsonr(press_matrix.T, press_matrix.T)
-plt.imshow(pcorr, vmin=0, vmax=1)
-plt.colorbar()
-##############################################################################
-# extract validated sessions
-##############################################################################
-directory='H:/Maxime/Hall Sensor Data/CATEG'
-mouse=4407
-skip=['20220208', '20220214', '20220222', '20220218', '20220207']
-Dates=os.listdir(directory)
-test=[]
-for date in Dates:
-    print(date)
-    if date in skip:
-        continue
-    file=[x for x in os.listdir(os.path.join(directory,date)) if str(mouse) in x][0]
-    filename=os.path.join(directory,date, file)
+# plt.imshow( press_matrix)
+# pcorr = manual_pairwise_pearsonr(press_matrix.T, press_matrix.T)
+# plt.imshow(pcorr, vmin=0, vmax=1)
+# plt.colorbar()
+# ##############################################################################
+# # extract validated sessions
+# ##############################################################################
+# directory='H:/Maxime/Hall Sensor Data/CATEG'
+# mouse=4407
+# skip=['20220208', '20220214', '20220222', '20220218', '20220207']
+# Dates=os.listdir(directory)
+# test=[]
+# for date in Dates:
+#     print(date)
+#     if date in skip:
+#         continue
+#     file=[x for x in os.listdir(os.path.join(directory,date)) if str(mouse) in x][0]
+#     filename=os.path.join(directory,date, file)
     
-    Data=pd.read_csv(filename)
-    Data.columns=['Hall_sensor','Magnet_block','Time']
-    Data, new_Down_idx, new_Up_idx = Get_press_indices(Data, master_df, mouse, date)
+#     Data=pd.read_csv(filename)
+#     Data.columns=['Hall_sensor','Magnet_block','Time']
+#     Data, new_Down_idx, new_Up_idx = Get_press_indices(Data, master_df, mouse, date)
         
     
     
     
-    #length_of_vector=int(np.mean(np.subtract(new_Up_idx,new_Down_idx)) + np.std(np.subtract(new_Up_idx,new_Down_idx)))
-    length_of_vector=1000
-    press_matrix=np.zeros((len(new_Down_idx),length_of_vector ))
-    magnet=Data.Hall_sensor.values
-    for i,(pressDown,pressUp) in enumerate(zip(new_Down_idx, new_Up_idx)):
-        if pressUp-pressDown>length_of_vector:
-            stop=length_of_vector
-        else:
-            stop=pressUp-pressDown
+#     #length_of_vector=int(np.mean(np.subtract(new_Up_idx,new_Down_idx)) + np.std(np.subtract(new_Up_idx,new_Down_idx)))
+#     length_of_vector=1000
+#     press_matrix=np.zeros((len(new_Down_idx),length_of_vector ))
+#     magnet=Data.Hall_sensor.values
+#     for i,(pressDown,pressUp) in enumerate(zip(new_Down_idx, new_Up_idx)):
+#         if pressUp-pressDown>length_of_vector:
+#             stop=length_of_vector
+#         else:
+#             stop=pressUp-pressDown
             
-        press_matrix[i,:stop]=magnet[pressDown:pressDown+stop]
+#         press_matrix[i,:stop]=magnet[pressDown:pressDown+stop]
     
-    temp_test=[]
-    for date in Dates:
-        print(date + '...')
-        #plt.figure()
-        if date in skip:
-            continue
-        file=[x for x in os.listdir(os.path.join(directory,date)) if str(mouse) in x][0]
-        filename=os.path.join(directory,date, file)
+#     temp_test=[]
+#     for date in Dates:
+#         print(date + '...')
+#         #plt.figure()
+#         if date in skip:
+#             continue
+#         file=[x for x in os.listdir(os.path.join(directory,date)) if str(mouse) in x][0]
+#         filename=os.path.join(directory,date, file)
         
-        Data=pd.read_csv(filename)
-        Data.columns=['Hall_sensor','Magnet_block','Time']
-        Data, new_Down_idx, new_Up_idx = Get_press_indices(Data, master_df, mouse, date)
+#         Data=pd.read_csv(filename) 
+#         Data.columns=['Hall_sensor','Magnet_block','Time']
+#         Data, new_Down_idx, new_Up_idx = Get_press_indices(Data, master_df, mouse, date)
             
-        #length_of_vector=int(np.mean(np.subtract(new_Up_idx,new_Down_idx)) + np.std(np.subtract(new_Up_idx,new_Down_idx)))
-        length_of_vector=1000
-        press_matrix1=np.zeros((len(new_Down_idx),length_of_vector ))
-        magnet=Data.Hall_sensor.values
-        for i,(pressDown,pressUp) in enumerate(zip(new_Down_idx, new_Up_idx)):
-            if pressUp-pressDown>length_of_vector:
-                stop=length_of_vector
-            else:
-                stop=pressUp-pressDown
+#         #length_of_vector=int(np.mean(np.subtract(new_Up_idx,new_Down_idx)) + np.std(np.subtract(new_Up_idx,new_Down_idx)))
+#         length_of_vector=1000
+#         press_matrix1=np.zeros((len(new_Down_idx),length_of_vector ))
+#         magnet=Data.Hall_sensor.values
+#         for i,(pressDown,pressUp) in enumerate(zip(new_Down_idx, new_Up_idx)):
+#             if pressUp-pressDown>length_of_vector:
+#                 stop=length_of_vector
+#             else:
+#                 stop=pressUp-pressDown
                 
-            press_matrix1[i,:stop]=magnet[pressDown:pressDown+stop]
+#             press_matrix1[i,:stop]=magnet[pressDown:pressDown+stop]
             
         
-        pcorr = manual_pairwise_pearsonr(press_matrix.T, press_matrix1.T)
+#         pcorr = manual_pairwise_pearsonr(press_matrix.T, press_matrix1.T)
         
     
-        flat_pcorr=np.ravel(pcorr)
-        drop_Ones=flat_pcorr[flat_pcorr!=1]
+#         flat_pcorr=np.ravel(pcorr)
+#         drop_Ones=flat_pcorr[flat_pcorr!=1]
         
-        # Cum=stats.cumfreq(drop_Ones, numbins=400, defaultreallimits=(-0.5,1.1))
-        # x= np.linspace(-0.5, Cum.binsize*Cum.cumcount.size-0.5, Cum.cumcount.size)
-        # #x= np.insert([np.log(a+1) for a in x][1:], 0,0)
-        # plt.plot(x,Cum.cumcount/np.size(drop_Ones), linestyle='dotted') #color=cmap(Cmap_index[j])
+#         # Cum=stats.cumfreq(drop_Ones, numbins=400, defaultreallimits=(-0.5,1.1))
+#         # x= np.linspace(-0.5, Cum.binsize*Cum.cumcount.size-0.5, Cum.cumcount.size)
+#         # #x= np.insert([np.log(a+1) for a in x][1:], 0,0)
+#         # plt.plot(x,Cum.cumcount/np.size(drop_Ones), linestyle='dotted') #color=cmap(Cmap_index[j])
         
-        temp_test.append(drop_Ones)
-    test.append(temp_test)
+#         temp_test.append(drop_Ones)
+#     test.append(temp_test)
 
 
-matrix=np.zeros((11,11))
-for i,each in enumerate(test):
-    for j,pcorr in enumerate(each):
-        matrix[i,j]=np.median(pcorr)
-        matrix[j,i]=np.median(pcorr)
-plt.imshow(matrix)    
-plt.colorbar()
-    #plt.figure()
-    plt.hist(np.ravel(pcorr ), bins=100, density=True, alpha=0.5)
-    # plt.figure()
-    # plt.imshow(pcorr, vmin=0, vmax=1)
-    # plt.colorbar()
+# matrix=np.zeros((11,11))
+# for i,each in enumerate(test):
+#     for j,pcorr in enumerate(each):
+#         matrix[i,j]=np.median(pcorr)
+#         matrix[j,i]=np.median(pcorr)
+#     plt.imshow(matrix)    
+#     plt.colorbar()
+#     #plt.figure()
+#     plt.hist(np.ravel(pcorr ), bins=100, density=True, alpha=0.5)
+#     # plt.figure()
+#     # plt.imshow(pcorr, vmin=0, vmax=1)
+#     # plt.colorbar()
 
     
 
-plt.plot(Data.Hall_sensor)
-plt.plot(cut_signal)    
+# plt.plot(Data.Hall_sensor)
+# plt.plot(cut_signal)    
 
-    
+
+# /Users/emma-fuze-grace/Lab/2022-02_TarVar_Categ_01/2022-02_TarVar_Categ_01_data
 def Get_press_indices(Data, master_df, mouse, date):
     from scipy.ndimage.filters import uniform_filter1d
     from scipy.fftpack import rfft, irfft, fftfreq    
@@ -231,14 +240,22 @@ def Get_press_indices(Data, master_df, mouse, date):
     #Normalize te entire trace
     Data.Hall_sensor=sp.stats.zscore(Data.Hall_sensor.values)
 
-   
-    
+    # Retrieve lever press time stamps
+    time_stamps=Data['Time'].values
+    mouse_df = master_df[master_df['Mouse'] == mouse]
+    lever_presses = mouse_df[mouse_df['Date'] == date]['Lever'].values[0]
+    x = [(1000 * press) + time_stamps[0] for press in lever_presses]
+    y = [rough_baseline + 25 for _ in range(len(lever_presses))]
+    plt.plot(x, y, '*--')
+    for press_num, press_time in enumerate(x):
+        plt.text(press_time, rough_baseline + 26, f'{press_num}')
     return Data, new_Down_idx, new_Up_idx
     
     
 # Build matrix 
 def adjust_press_idx(Data,Down_idx, Up_idx, plot=False):
     magnet=Data['Hall_sensor'].values
+    time_stamps=Data['Time'].values
     new_Down_idx=np.zeros_like(Down_idx[0])
     for j,idx in enumerate(Down_idx[0]):
         while np.mean(magnet[idx:idx+10])<=magnet[idx-1]:
@@ -256,11 +273,21 @@ def adjust_press_idx(Data,Down_idx, Up_idx, plot=False):
      
     if plot:
         plt.figure()
-        plt.plot(Data.Hall_sensor.values, alpha=0.5)
-        plt.scatter(new_Down_idx, magnet[new_Down_idx], c='b')
-        plt.scatter(new_Up_idx, magnet[new_Up_idx], c='r')
-        for [a,b, v] in zip (new_Down_idx, new_Up_idx, magnet[new_Down_idx]):
+        plt.plot(Data.Time.values, Data.Hall_sensor.values, alpha=0.5)
+        plt.scatter(time_stamps[new_Down_idx], magnet[new_Down_idx], c='b')
+        plt.scatter(time_stamps[new_Up_idx], magnet[new_Up_idx], c='r')
+        for [press_num, press_ind, a, b, v] in zip(range(len(new_Down_idx)),
+                                        new_Down_idx,
+                            time_stamps[new_Down_idx], 
+                            time_stamps[new_Up_idx], 
+                            magnet[new_Down_idx]):
             plt.hlines(v,a,b)
+            plt.text(a, v + 1, f'{press_num + 1}') # Plot predicted press num
+            plt.text(a, v - 2, f'{press_ind}', fontsize=8) # Plot predicted press num
+    for [press_ind, a, v] in zip(new_Up_idx,
+                           time_stamps[new_Up_idx], 
+                           magnet[new_Up_idx]):
+        plt.text(a, v - 2, f'{press_ind}', fontsize=8)
     return new_Down_idx, new_Up_idx
     
 
@@ -389,3 +416,26 @@ def manual_pairwise_pearsonr(A_array,B_array):
     # Finally compute Pearson Correlation Coefficient as 2D array 
     pcorr = ((p1 - p2)/np.sqrt(p4*p3[:,None]))
     return pcorr
+
+
+"""
+Plots to make
+5 x 5 within session
+10 x 10 by day (x5 per no# in press)
+                
+10x10 correlations:
+only presses in sequences
+only presses not in sequence
+"""
+
+if __name__ == '__main__':
+    from create_medpc_master import create_medpc_master
+    mice = [i for i in range(4386, 4414)]
+    file_dir = '/Users/emma-fuze-grace/Lab/Medpc Data'
+    master_df = create_medpc_master(mice, file_dir)
+    mouse = 4407
+    date = '20220209'
+    filename = f'/Users/emma-fuze-grace/Lab/Hall Sensor Data/HallSensor_{date}_{mouse}.CSV'
+    Data = pd.read_csv(filename)
+    Data.columns = ['Hall_sensor','Magnet_block','Time']
+    Data, new_Down_idx, new_Up_idx = Get_press_indices(Data, master_df, mouse, date)
